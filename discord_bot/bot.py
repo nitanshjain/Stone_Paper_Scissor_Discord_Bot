bc6e1bf6b072c7402196597fd0b3d0dba5c6f1be
nitanshjain <nitanshjain@gmail.com>
2022-03-17 13:00:40 +0530

    completed first command ~start

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -11,11 +11,6 @@ from discord.utils import get
 
 
 load_dotenv()
-# client=discord.Client()
-
-# connecting to sql database
-conn=sqlite3.connect('gamedb.sqlite')
-cur = conn.cursor()
 
 # dictionary with numbers being mapped to the three options
 game = {
@@ -24,88 +19,105 @@ game = {
     "scissor" : 2
 }
 
-
-
-
-cur.executescript('''              
-    CREATE TABLE IF NOT EXISTS Player (
-        player_id           INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-        player_name         TEXT UNIQUE     
-    );
-    
-    CREATE TABLE IF NOT EXISTS Round (
-        game_id             INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-        player_id           INTEGER,
-        player_input	    INTEGER,
-        computer_input	    INTEGER,
-        winner	            TEXT,
-        FOREIGN KEY (player_id) REFERENCES Player(player_id)
-    );
-''')
-
 #setting prefix for bot
 PREFIX=str(os.getenv('PREFIX'))
 bot = commands.Bot(command_prefix=PREFIX)
 
 @bot.event
 async def on_ready():
+    
+    # connecting to sql database
+    conn=sqlite3.connect('gamedb.sqlite')
+    cur = conn.cursor()
+    
+    
+    cur.executescript('''              
+        CREATE TABLE IF NOT EXISTS Player (
+            player_id           INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            player_name         TEXT UNIQUE     
+        );
+        
+        CREATE TABLE IF NOT EXISTS Round (
+            game_id             INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            player_id           INTEGER,
+            player_input	    INTEGER,
+            computer_input	    INTEGER,
+            winner	            TEXT,
+            FOREIGN KEY (player_id) REFERENCES Player(player_id)
+        );
+    ''')
+    
+    conn.commit()
+    conn.close()
     print('We have logged in as {0.user}'.format(bot))
 
 @bot.command(name='start', pass_context=True)
 async def startGame(ctx):
-    # print(dir(bot))
+    conn=sqlite3.connect('gamedb.sqlite')
+    cur = conn.cursor()
     
     # getting player name
     player_name=str(ctx.author)
     player_name=player_name.strip()
-    await bot.channel.send(player_name)
-#     # # inputting player name in Player table
-#     # cur.execute('''INSERT OR IGNORE INTO Player (player_name) VALUES (?)''',(player_name,))
-#     # cur.execute('SELECT player_id FROM Player WHERE player_name= ?', (player_name,))
+    await ctx.send('You will now play {}'.format(player_name))
+    
+    # inputting player name in Player table
+    cur.execute('''INSERT OR IGNORE INTO Player (player_name) VALUES (?)''',(player_name,))
+    cur.execute('SELECT player_id FROM Player WHERE player_name= ?', (player_name,))
+    await ctx.send('{} added to database'.format(player_name))
+    
+    # fetching player_id from player_name
+    player_id=cur.fetchone()[0]
 
-#     # # fetching player_id from player_name
-#     # player_id=cur.fetchone()[0]
+    # creating list of all options
+    game_list=list(game.items())
 
-#     # # creating list of all options
-#     # game_list=list(game.items())
+    # selecting random option from game_list and taking player_input
+    rand_choice=random.choice(game_list)
+    await ctx.send('What do you want to choose ? ')
+    
+    # taking user input and storing it in variable
+    try:
+        msg = await bot.wait_for("message", timeout=30) # 30 seconds to reply
+    except asyncio.TimeoutError:
+        await ctx.send('Sorry, bot took your delay as a sign of cowardice')
+    await ctx.send('Your option was {}'.format(msg.content))
+    player_choice=str(msg.content)
+    player_choice=player_choice.lower();
 
-#     # # selecting random option from game_list and taking player_input
-#     # rand_choice=random.choice(game_list)
-#     # player_choice=input("What do you want to choose ? ")
-#     # player_choice=player_choice.lower();
 
 
 
-
-#     # if player_choice in game.keys():
+    if player_choice in game.keys():
         
-#     #     # on draw
-#     #     if player_choice==rand_choice[0]:
-#     #         print("Computer choose", rand_choice[0],". Its A Draw")
-#     #         winner=nan
-#     #         cur.execute('''INSERT OR IGNORE INTO Round (player_id, player_input, computer_input, winner) 
-#     #                         VALUES (?,?,?,? )''',
-#     #                         (player_id, player_choice, rand_choice[0], winner,))
-#     #     # on player winning 
-#     #     elif (player_choice=='stone' and rand_choice[0]=='scissor') or (player_choice=='scissor' and rand_choice[0]=='paper') or (player_choice=='paper' and rand_choice[0]=='stone'):
-#     #         print("Computer choose",rand_choice[0],". You Won")
-#     #         winner=player_name
-#     #         cur.execute('''INSERT OR IGNORE INTO Round (player_id, player_input, computer_input, winner) 
-#     #                         VALUES (?,?,?,? )''',
-#     #                         (player_id, player_choice, rand_choice[0], winner,))
-#     #     # on computer winning 
-#     #     else:
-#     #         print("Computer choose",rand_choice[0],". You Lost")
-#     #         winner="Computer"
-#     #         cur.execute('''INSERT OR IGNORE INTO Round (player_id, player_input, computer_input, winner) 
-#     #                         VALUES (?,?,?,? )''',
-#     #                         (player_id, player_choice, rand_choice[0], winner,))
+        # on draw
+        if player_choice==rand_choice[0]:
+            await ctx.send('Bot choose {} . Its A Draw'.format(rand_choice[0]))
+            winner=nan
+            cur.execute('''INSERT OR IGNORE INTO Round (player_id, player_input, computer_input, winner) 
+                            VALUES (?,?,?,? )''',
+                            (player_id, player_choice, rand_choice[0], winner,))
+        # on player winning 
+        elif (player_choice=='stone' and rand_choice[0]=='scissor') or (player_choice=='scissor' and rand_choice[0]=='paper') or (player_choice=='paper' and rand_choice[0]=='stone'):
+            await ctx.send('Bot choose {} . {} Won '.format(rand_choice[0],player_name))
+            winner=player_name
+            cur.execute('''INSERT OR IGNORE INTO Round (player_id, player_input, computer_input, winner) 
+                            VALUES (?,?,?,? )''',
+                            (player_id, player_choice, rand_choice[0], winner,))
+        # on computer winning 
+        else:
+            await ctx.send('Bot choose {} . {} Won '.format(rand_choice[0],player_name))
+            winner="Computer"
+            cur.execute('''INSERT OR IGNORE INTO Round (player_id, player_input, computer_input, winner) 
+                            VALUES (?,?,?,? )''',
+                            (player_id, player_choice, rand_choice[0], winner,))
 
-#     # else:
-#     #     print("Invalid Input")
+    else:
+        await ctx.send('We are playing stone, paper and scissor. What are you playing ?')
+    conn.commit()
+    conn.close()
 
-# conn.commit()
-# conn.close()
+
 
 
 D_TOKEN=os.getenv('TOKEN')
